b4e7d48ba037d56ee2ebda72f86922f3b9b73c09
Sonali Kalra <sonalikalra2994@gmail.com>
2025-09-26 16:57:45 +0530

    fixed logic for daily challenge task using redux

--- src/DailyChallenge.js
+++ src/DailyChallenge.js
@@ -1,9 +1,14 @@
 import react from "react";
 import Game from './Game';
+import { useDispatch } from "react-redux";
+import { setCategory, setSize } from "./gameSlice";
 
 const DailyChallengeGame = () => {
-    const category = undefined;
-    const size = {rows: 9, cols: 6}
+    const dispatch = useDispatch();
+    const category = null;
+    const size = {rows: 9, cols: 6};
+    dispatch(setCategory(category));
+    dispatch(setSize(size))
     return (
         <Game category = {category}  size = {size} />
     )
